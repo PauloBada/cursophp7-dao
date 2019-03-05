59f8425ff4fec3fba3abfdf57edc08eced61ce06
Paulo T Jaques <bada305@gmail.com>
2019-03-05 14:45:54 -0300

    Incluidas functions getList(), search() e login() em Usuario.php

--- class/Usuario.php
+++ class/Usuario.php
@@ -1,5 +1,6 @@
 <?php
 
+// Poderia se chamar "PersisteUsuario.php", pois abaixo dos geters e seters ficariam os selects
 class Usuario {
 	private $idusuario;
 	private $deslogin;
@@ -38,6 +39,43 @@ class Usuario {
 		$this->dtcadastro = $dataTime;
 	}
 
+	// Pode-se crir "n"	 functions que seriam os selects e esta classe poderia se chamar PersisteUsuario.php
+	
+	// "static", pois não utiliza "this"
+	// Retorna uma lista de registros
+	public static function getList() {
+		$sql = new Sql();
+
+		return $sql->select("SELECT * FROM tb_usuarios ORDER BY idusuario desc");
+	}
+
+// Retorna uma lista de registros que contenham o texto pesquisado
+	public static function search($txtPesq) {
+		$sql = new Sql();
+		return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin DESC", array(
+		':SEARCH'=>"%".$txtPesq."%"));	
+	}
+
+	// Retorna somente um registro
+	public function login($login, $senha) {
+		$sql = new Sql();
+
+		$results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :SENHA", array(
+			":LOGIN"=>$login, ":SENHA"=>$senha));
+
+		if (count($results) > 0) {
+			$row = $results[0];
+
+			$this->setIdusuario($row['idusuario']);
+			$this->setDeslogin($row['deslogin']);
+			$this->setDessenha($row['dessenha']);
+			$this->setDtcadastro(new DateTime($row['dtcadastro']));
+		} else {
+			throw new Exception("Login ou senha inválido(s)", 1);			
+		}
+	}
+
+	// Retorna somente um registro
 	public function loadById($id) {
 		$sql = new Sql();
 
@@ -51,29 +89,43 @@ class Usuario {
 			$this->setDeslogin($row['deslogin']);
 			$this->setDessenha($row['dessenha']);
 			$this->setDtcadastro(new DateTime($row['dtcadastro']));
-		} 
+		} else {
+			throw new Exception("Usuário Não Localizado!", 1);			
+		}
 	}
 
 	// Esta função (método) sempre será executada, tendo encontrado o registro ou não!
-	public function __toString() {
-		if ($this->getIdusuario() !== null) {
-			return json_encode(array(
-				"idusuario"=>$this->getIdusuario(), 
-				"deslogin"=>$this->getDeslogin(),
-				"dessenha"=>$this->getDessenha(),
-				"dtcadastro"=>$this->getDtcadastro()->format("d/m/Y H:i:s")
-			));
-		} 
-		else {
-			// return json_encode(array(0, "Registro Nao localizado!", 0, 0));
-			return json_encode(array(
-				"idusuario"=>"0", 
-				"deslogin"=>"0",
-				"dessenha"=>"0",
-				"dtcadastro"=>0
-			));
-		}
+
+	// Lógica feita antes de aprender o comando "throw new Excpetion" acima
+	// public function __toString() {
+	// 	if ($this->getIdusuario() !== null) {
+	// 		return json_encode(array(
+	// 			"idusuario"=>$this->getIdusuario(), 
+	// 			"deslogin"=>$this->getDeslogin(),
+	// 			"dessenha"=>$this->getDessenha(),
+	// 			"dtcadastro"=>$this->getDtcadastro()->format("d/m/Y H:i:s")
+	// 		));
+	// 	} 
+	// 	else {
+	// 		// return json_encode(array(0, "Registro Nao localizado!", 0, 0));
+	// 		return json_encode(array(
+	// 			"idusuario"=>"0", 
+	// 			"deslogin"=>"0",
+	// 			"dessenha"=>"0",
+	// 			"dtcadastro"=>0
+	// 		));
+	// 	}
+	// }
+
+	public function __toString() {	
+		return json_encode(array(
+			"idusuario"=>$this->getIdusuario(), 
+			"deslogin"=>$this->getDeslogin(),
+			"dessenha"=>$this->getDessenha(),
+			"dtcadastro"=>$this->getDtcadastro()->format("d/m/Y H:i:s")
+		));
 	}
+
 }
 
 ?>
--- index.php
+++ index.php
@@ -1,24 +1,47 @@
+
 <?php
 
 require_once("config.php");
+
 
 // Substituídos pela classe "Usuarios.php"
 // $sql = new Sql();
 // $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 // echo json_encode($usuarios);
 
+// ==== Busca usuário específico
 $pesquisa = new Usuario();
-$idPesq = 4;
+$idPesq = 24;
 $pesquisa->loadById($idPesq);
+echo $pesquisa;				
 
-$volta = json_decode($pesquisa, true);
+// Lógica feita antes de aprender o comando "throw new Excpetion" em Usuario.php
+// $pesquisa = new Usuario();
+// $idPesq = 24;
+// $pesquisa->loadById($idPesq);
+// $volta = json_decode($pesquisa, true);
+// if ($volta["idusuario"] != "0") {
+// 	echo $pesquisa;				
+// } else {
+// 	echo "Registro $idPesq não localizado! Verifique!";
+// }
 
-if ($volta["idusuario"] != "0") {
-	echo $pesquisa;				
-} else {
-	echo "Registro $idPesq não localizado! Verifique!";
-}
+// ===== Busca todos os usuários
+// Como getList() não retorna variáveis através de "this", deve-se chamar direto e não deve instanciar com "new"
+// $pesquisa = Usuario::getList();
+// echo json_encode($pesquisa);				
+
+// ===== Busca todos os usuários que tenham determinado texto
+// $pesquisa = Usuario::search("Tarr");
+// echo json_encode($pesquisa);				
+
+// ===== Busca usuário por usuário e senha
+// $pesquisa = new Usuario();
+// $loginPesq = "PAULO TARRAGO";
+// $senhaPesq = 'tutasi';
+// $pesquisa->login($loginPesq, $senhaPesq);
+// echo $pesquisa;				
 
 
- 
+
 ?>
